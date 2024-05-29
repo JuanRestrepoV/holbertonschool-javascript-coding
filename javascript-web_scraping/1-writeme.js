1acd0466fe51e61a20308b8896b00841b3f7d049
Juan Pablo <juanpablorestrepovidal@gmail.com>
2024-05-29 14:17:12 -0500

    task 1

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 const fs = require('fs');
 
-const string = process.argv[3];
 const filePath = process.argv[2];
+const string = process.argv[3];
 
 fs.writeFile(filePath, string, 'utf8', (err, data) => {
   if (err) console.error(err);
-  else return(string);
+  else return (string);
 });
