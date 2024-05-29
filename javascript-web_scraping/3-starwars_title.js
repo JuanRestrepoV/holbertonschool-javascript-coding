588fa8a2bafdf8769edbdf4bbb525be6b092956a
Juan Pablo <juanpablorestrepovidal@gmail.com>
2024-05-29 15:16:31 -0500

    task 3

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -2,9 +2,9 @@
 
 const request = require('request');
 
-const movie_ID = process.argv[2];
+const Movie_ID = process.argv[2];
 
-const apiUrl = `https://swapi-api.hbtn.io/api/films/${movie_ID}`;
+const apiUrl = `https://swapi-api.hbtn.io/api/films/${Movie_ID}`;
 
 request(apiUrl, (error, response, body) => {
   if (error) {
