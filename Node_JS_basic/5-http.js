90f618b106eafeed3a0dc0d0815266691ad0e13f
Juan Pablo <juanpablorestrepovidal@gmail.com>
2024-06-14 18:26:07 -0500

    task 5

---
+++ Node_JS_basic/5-http.js
@@ -0,0 +1,53 @@
+const { createServer } = require('http');
+const fs = require('fs');
+
+const port = 1245;
+
+const app = createServer((req, res) => {
+  res.setHeader('Content-Type', 'text/plain');
+
+  if (req.url === '/') {
+    res.statusCode = 200;
+    res.end('Hello Holberton School!');
+  } else if (req.url === '/students') {
+    fs.readFile(process.argv[2], 'utf-8', (err, data) => {
+      if (err) {
+        res.statusCode = 500;
+        res.end('This is the list of our students\nCannot load the database');
+        return;
+      }
+
+      const lines = data.trim().split('\n').filter((line) => line);
+      const students = {};
+      let totalStudents = 0;
+
+      for (let i = 1; i < lines.length; i++) {
+        const [name, , , field] = lines[i].split(',');
+        if (!students[field]) students[field] = [];
+        students[field].push(name);
+        totalStudents++;
+      }
+
+      const response = [
+        'This is the list of our students',
+        `Number of students: ${totalStudents}`,
+      ];
+
+      for (const [field, names] of Object.entries(students)) {
+        response.push(`Number of students in ${field}: ${names.length}. List: ${names.join(', ')}`);
+      }
+
+      res.statusCode = 200;
+      res.end(response.join('\n'));
+    });
+  } else {
+    res.statusCode = 404;
+    res.end('Not Found');
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
+
+module.exports = app;
